633b80f1216733e17f1bfd755dd606d26407ebbb
leander-ms <leander.tripp@gmail.com>
2024-02-13 10:26:48 +0100

    Changed identation to four spaces

--- fakeslam.py
+++ fakeslam.py
@@ -12,47 +12,46 @@ from renderer import Renderer
 from display import Display2D, Display3D
 
 if __name__ == "__main__":
-  W,H = 640,480
-  F = H  # with 45 degree FoV
-  
-  K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
-  Kinv = np.linalg.inv(K)
+    W,H = 640,480
+    F = H  # with 45 degree FoV
 
-  disp3d = Display3D()
-  disp2d = Display2D(W, H)
+    K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+    Kinv = np.linalg.inv(K)
 
-  slam = SLAM(W, H, K)
-  r = Renderer(W, H)
+    disp3d = Display3D()
+    disp2d = Display2D(W, H)
 
-  fn = 0
-  pos_x = 0
-  dir_x = True
-  while 1:
-    fn += 1
+    slam = SLAM(W, H, K)
+    r = Renderer(W, H)
 
-    # render
-    frame, verts = r.draw([pos_x,0,0])
+    fn = 0
+    pos_x = 0
+    dir_x = True
+    while 1:
+        fn += 1
 
-    # add gaussian noise
-    verts += np.random.normal(0.0, 0.1, verts.shape)
+        # render
+        frame, verts = r.draw([pos_x,0,0])
 
-    # ground truth pose
-    pose = np.eye(4)
-    pose[0,3] = pos_x
+        # add gaussian noise
+        verts += np.random.normal(0.0, 0.1, verts.shape)
 
-    slam.process_frame(frame, None, verts)
-    disp3d.paint(slam.mapp)
+        # ground truth pose
+        pose = np.eye(4)
+        pose[0,3] = pos_x
 
-    img = slam.mapp.frames[-1].annotate(frame)
-    disp2d.paint(img)
+        slam.process_frame(frame, None, verts)
+        disp3d.paint(slam.mapp)
 
-    # flip flop
-    if pos_x > 10:
-      dir_x = False
-    elif pos_x < -10:
-      dir_x = True
-    pos_x += 0.5 * (1 if dir_x else -1)
+        img = slam.mapp.frames[-1].annotate(frame)
+        disp2d.paint(img)
 
-    if fn > 20:
-      slam.mapp.optimize(verbose=True, local_window=None, rounds=10)
+        # flip flop
+        if pos_x > 10:
+            dir_x = False
+        elif pos_x < -10:
+            dir_x = True
+        pos_x += 0.5 * (1 if dir_x else -1)
 
+        if fn > 20:
+            slam.mapp.optimize(verbose=True, local_window=None, rounds=10)
